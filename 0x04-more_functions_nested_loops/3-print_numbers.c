91a725b6f2d315f49d415c6befca5bc522b65ea0
Dagmawi-Kebede <dagmawikebededev@outlook.com>
2023-03-24 06:10:59 -0700

    function program 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -7,3 +7,5 @@ Programs
 1-isdigit.c , checks for a digit(0 through 9) and returns 1 if c is a digit, Otherwise returns 0.
 
 2-mul.c , multiplies two integers.
+
+3-print_numbers.c ,  prints the numbers, from 0 to 9.

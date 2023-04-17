ea1f2df5adc25393cff578b33f782ce557b128b8
Dagmawi-Kebede <dagmawikebededev@outlook.com>
2023-04-17 13:06:05 -0700

    structures 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,10 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - define a new type dog_t as a new name for the
+ * type struct dog.
+ */
+typedef struct dog dog_t;
+
 #endif

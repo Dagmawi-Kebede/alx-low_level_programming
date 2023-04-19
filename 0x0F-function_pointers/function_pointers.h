b11d486644284238b4635042730f469896d39504
Dagmawi-Kebede <dagmawikebededev@outlook.com>
2023-04-19 13:35:37 -0700

    pointer program 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
